555b0412646abc90950bd72b0fe1de89b701e7e9
Carlos Dauden <carlos.dauden@tecnativa.com>
2026-05-16 11:52:24 +0200

    [IMP] shoppingfeed_integration: Performance and memory consumption

--- shoppingfeed_integration/controllers/catalog.py
+++ shoppingfeed_integration/controllers/catalog.py
@@ -1,6 +1,7 @@
 # Copyright 2025 Juan Carlos Oñate - Tecnativa <juancarlos.onate@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import tempfile
 from datetime import datetime
 
 from lxml import etree
@@ -36,20 +37,15 @@ class CatalogController(http.Controller):
                 status=404,
                 content_type="application/xml;charset=utf-8",
             )
-        products = self._get_products_for_store(store)
-        base_url = store.get_base_url().rstrip("/")
-        catalog_el = self._build_catalog_xml(store, products, base_url)
-        xml_bytes = etree.tostring(
-            catalog_el, pretty_print=True, xml_declaration=True, encoding="UTF-8"
-        )
+        catalog_file = self._generate_catalog_file(store)
         return Response(
-            xml_bytes, content_type="application/xml;charset=utf-8", status=200
+            catalog_file,
+            content_type="application/xml;charset=utf-8",
+            status=200,
+            direct_passthrough=True,
         )
 
-    def _get_products_for_store(self, store):
-        env = request.env["product.product"].sudo().with_company(store.company_id)
-        if store.lang_id:
-            env = env.with_context(lang=store.lang_id.code)
+    def _get_product_domain_for_store(self, store):
         domain = []
         if store.export_only_selected:
             domain.append(("export_to_shoppingfeed", "=", True))
@@ -71,25 +67,184 @@ class CatalogController(http.Controller):
             domain.append(("sale_ok", "=", True))
         if store.allowed_categ_ids:
             domain.append(("public_categ_ids", "in", store.allowed_categ_ids.ids))
-        return env.search(domain)
+        return domain
+
+    def _get_product_env_for_store(self, store):
+        env = request.env["product.product"].sudo().with_company(store.company_id)
+        if store.lang_id:
+            env = env.with_context(lang=store.lang_id.code)
+        return env
+
+    def _get_products_for_store(self, store):
+        return self._get_product_env_for_store(store).search(
+            self._get_product_domain_for_store(store)
+        )
+
+    def _generate_catalog_file(self, store):
+        catalog_file = tempfile.TemporaryFile()
+        for chunk in self._iter_catalog_xml(store):
+            catalog_file.write(chunk)
+        catalog_file.seek(0)
+        return catalog_file
+
+    def _iter_catalog_xml(self, store, batch_size=500):
+        product_env = self._get_product_env_for_store(store)
+        domain = self._get_product_domain_for_store(store)
+        product_ids = product_env.search(domain).ids
+        total_products = len(product_ids)
+        generation_started_at = datetime.now()
+        catalog_context = self._get_catalog_context(store)
+
+        yield b'<?xml version="1.0" encoding="UTF-8"?>\n'
+        yield b'<catalog>\n<products version="1.0.0">\n'
+        for index in range(0, total_products, batch_size):
+            batch = product_env.browse(product_ids[index : index + batch_size])
+            self._fetch_product_batch_fields(store, batch)
+            batch_context = self._get_product_batch_context(store, batch)
+            for product in batch:
+                product_el = self._build_product_xml(
+                    store, product, catalog_context, batch_context
+                )
+                yield etree.tostring(product_el, encoding="UTF-8")
+                yield b"\n"
+            request.env.cache.invalidate()
+        yield b"</products>\n"
+        metadata_el = self._build_metadata_xml(total_products, generation_started_at)
+        yield etree.tostring(metadata_el, encoding="UTF-8")
+        yield b"\n</catalog>\n"
+
+    def _get_catalog_context(self, store):
+        return {
+            "allowed_categ_ids": set(store.allowed_categ_ids.ids),
+            "base_url": store.get_base_url().rstrip("/"),
+        }
+
+    def _fetch_product_batch_fields(self, store, products):
+        field_names = [
+            "barcode",
+            "default_code",
+            "description_sale",
+            "lst_price",
+            "name",
+            "product_brand_id",
+            "product_template_attribute_value_ids",
+            "product_template_image_ids",
+            "product_tmpl_id",
+            "public_categ_ids",
+            "sale_ok",
+            "sf_forced_category_id",
+            "taxes_id",
+            "type",
+            "website_description",
+            "website_url",
+            "weight",
+        ]
+        if store.custom_sku_field_id:
+            field_names.append(store.custom_sku_field_id.name)
+        field_names.extend(
+            attr_field.field_id.name
+            for attr_field in store.additional_attribute_field_ids
+        )
+        products.fetch(set(field_names))
+
+    def _get_product_batch_context(self, store, products):
+        base_prices = self._get_base_prices(store, products)
+        quantity_values = self._get_quantity_values(store, products)
+        main_images = self._get_main_images(products)
+        additional_prices = {}
+        for pricelist in store.additional_pricelist_ids:
+            if not pricelist.shoppingfeed_attribute_name:
+                continue
+            prices = pricelist._get_products_price(products, 1.0, None)
+            additional_prices[pricelist.id] = {
+                product_id: pricelist.currency_id.round(price)
+                for product_id, price in prices.items()
+            }
+        return {
+            "additional_prices": additional_prices,
+            "base_prices": base_prices,
+            "main_images": main_images,
+            "quantity_values": quantity_values,
+        }
+
+    def _get_base_prices(self, store, products):
+        currency = (
+            store.pricelist_id.currency_id
+            if store.pricelist_id
+            else store.company_id.currency_id
+        )
+        if store.pricelist_id:
+            prices = store.pricelist_id._get_products_price(products, 1.0, None)
+        else:
+            prices = {product.id: product.lst_price or 0.0 for product in products}
+        return {
+            product_id: currency.round(price) for product_id, price in prices.items()
+        }
+
+    def _get_quantity_values(self, store, products):
+        if not store.use_actual_stock_state:
+            return {}
+        if store.quantity_type == "virtual":
+            return {
+                data["id"]: data["virtual_available"]
+                for data in products.read(["virtual_available"])
+            }
+        return dict(products._get_only_qty_available())
+
+    def _get_main_images(self, products):
+        products_data = products.with_context(bin_size=True).read(
+            ["image_variant_1920", "product_tmpl_id"]
+        )
+        template_ids = {
+            data["product_tmpl_id"][0]
+            for data in products_data
+            if data["product_tmpl_id"] and not data["image_variant_1920"]
+        }
+        template_images = {
+            data["id"]: bool(data["image_1920"])
+            for data in products.env["product.template"]
+            .browse(template_ids)
+            .with_context(bin_size=True)
+            .read(["image_1920"])
+        }
+        return {
+            data["id"]: bool(data["image_variant_1920"])
+            or template_images.get(data["product_tmpl_id"][0], False)
+            for data in products_data
+        }
 
     def _build_catalog_xml(self, store, products, base_url):
         catalog_el = etree.Element("catalog")
         products_el = etree.SubElement(catalog_el, "products", version="1.0.0")
+        catalog_context = self._get_catalog_context(store)
+        if base_url:
+            catalog_context["base_url"] = base_url.rstrip("/")
+        batch_context = self._get_product_batch_context(store, products)
         for product in products:
-            product_el = etree.SubElement(products_el, "product")
-            self._add_product_base_info(store, product_el, product, base_url)
-            self._add_product_price(store, product_el, product)
-            self._add_product_stock(store, product_el, product)
-            self._add_product_media(store, product_el, product, base_url)
-            self._add_product_attributes(store, product_el, product)
+            products_el.append(
+                self._build_product_xml(store, product, catalog_context, batch_context)
+            )
         self._add_metadata(catalog_el, len(products))
         return catalog_el
 
-    def _get_base_price(self, store, product):
-        price = product.lst_price or 0.0
-        if store.pricelist_id:
-            price = store.pricelist_id._get_product_price(product, 1.0, None)
+    def _build_product_xml(self, store, product, catalog_context, batch_context):
+        product_el = etree.Element("product")
+        self._add_product_base_info(store, product_el, product, catalog_context)
+        self._add_product_price(store, product_el, product, batch_context)
+        self._add_product_stock(store, product_el, product, batch_context)
+        self._add_product_media(
+            store, product_el, product, catalog_context, batch_context
+        )
+        self._add_product_attributes(store, product_el, product, batch_context)
+        return product_el
+
+    def _get_base_price(self, store, product, batch_context=None):
+        if batch_context:
+            price = batch_context["base_prices"].get(product.id, 0.0)
+        else:
+            price = product.lst_price or 0.0
+            if store.pricelist_id:
+                price = store.pricelist_id._get_product_price(product, 1.0, None)
         currency = (
             store.pricelist_id.currency_id
             if store.pricelist_id
@@ -97,7 +252,8 @@ class CatalogController(http.Controller):
         )
         return currency.round(price)
 
-    def _add_product_base_info(self, store, product_el, product, base_url):
+    def _add_product_base_info(self, store, product_el, product, catalog_context):
+        base_url = catalog_context["base_url"]
         if store.use_product_id_as_sku:
             sku_value = str(product.id)
         elif store.custom_sku_field_id:
@@ -125,7 +281,7 @@ class CatalogController(http.Controller):
             category = product.sf_forced_category_id
         else:
             category = product.public_categ_ids.filtered(
-                lambda categ: categ.id in store.allowed_categ_ids.ids
+                lambda categ: categ.id in catalog_context["allowed_categ_ids"]
             )[:1]
         if category:
             category_el = etree.SubElement(product_el, "category")
@@ -141,8 +297,8 @@ class CatalogController(http.Controller):
         short_desc = product.description_sale or ""
         etree.SubElement(description_el, "short").text = etree.CDATA(short_desc)
 
-    def _add_product_price(self, store, product_el, product):
-        price = self._get_base_price(store, product)
+    def _add_product_price(self, store, product_el, product, batch_context=None):
+        price = self._get_base_price(store, product, batch_context)
         if store.include_taxes_in_price and product.taxes_id:
             currency = (
                 store.pricelist_id.currency_id
@@ -156,30 +312,37 @@ class CatalogController(http.Controller):
             price = currency.round(tax_result["total_included"])
         etree.SubElement(product_el, "price").text = str(price)
 
-    def _get_quantity_value(self, store, product):
+    def _get_quantity_value(self, store, product, batch_context=None):
+        if batch_context:
+            return batch_context["quantity_values"].get(product.id, 0.0)
         if store.quantity_type == "virtual":
             return product.virtual_available
         return product.qty_available
 
-    def _add_product_stock(self, store, product_el, product):
+    def _add_product_stock(self, store, product_el, product, batch_context=None):
         if not store.use_actual_stock_state:
             quantity_value = store.default_quantity
         else:
-            quantity_value = self._get_quantity_value(store, product) or 0
+            quantity_value = (
+                self._get_quantity_value(store, product, batch_context) or 0
+            )
             if product.type == "service":
                 quantity_value = store.default_quantity
         if not product.sale_ok and store.force_zero_quantity_non_salable:
             quantity_value = 0
         etree.SubElement(product_el, "quantity").text = str(int(quantity_value))
 
-    def _add_product_media(self, store, product_el, product, base_url):
+    def _add_product_media(
+        self, store, product_el, product, catalog_context, batch_context=None
+    ):
+        base_url = catalog_context["base_url"]
         images_el = etree.SubElement(product_el, "images")
         product_images = product.product_template_image_ids
         if store.export_all_images:
             all_images = product_images
         else:
             all_images = product_images[: store.exported_image_count]
-        if product.image_1920:
+        if self._has_main_image(product, batch_context):
             etree.SubElement(images_el, "image", type="main").text = etree.CDATA(
                 f"{base_url}/web/image/product.product/{product.id}/image_1920"
             )
@@ -188,7 +351,12 @@ class CatalogController(http.Controller):
                 f"{base_url}/web/image/{img._name}/{img.id}/image_1920"
             )
 
-    def _add_product_attributes(self, store, product_el, product):
+    def _has_main_image(self, product, batch_context=None):
+        if batch_context:
+            return batch_context["main_images"].get(product.id, False)
+        return self._get_main_images(product).get(product.id, False)
+
+    def _add_product_attributes(self, store, product_el, product, batch_context=None):
         attributes_el = etree.SubElement(product_el, "attributes")
         # Attributes that generate variants
         for value in product.product_template_attribute_value_ids:
@@ -204,8 +372,10 @@ class CatalogController(http.Controller):
             etree.SubElement(attr_el, "value").text = value.name
         # Attributes that do NOT generate variants
         for line in product.product_tmpl_id.attribute_line_ids.filtered(
-            lambda line_var: line_var.attribute_id.create_variant == "no_variant"
-            and line_var.attribute_id.shoppingfeed_export
+            lambda line_var: (
+                line_var.attribute_id.create_variant == "no_variant"
+                and line_var.attribute_id.shoppingfeed_export
+            )
         ):
             if line.value_ids:
                 values = ", ".join(line.value_ids.mapped("name"))
@@ -243,12 +413,14 @@ class CatalogController(http.Controller):
                     etree.SubElement(attr_el, "value").text = etree.CDATA(str(value))
                 else:
                     etree.SubElement(attr_el, "value").text = str(value)
-        self._add_product_price_attributes(store, attributes_el, product)
+        self._add_product_price_attributes(store, attributes_el, product, batch_context)
 
-    def _add_product_price_attributes(self, store, attributes_el, product):
+    def _add_product_price_attributes(
+        self, store, attributes_el, product, batch_context=None
+    ):
         # Export price without taxes as additional attribute for marketplaces
         if store.export_price_without_tax and store.price_without_tax_attribute_name:
-            price_without_tax = self._get_base_price(store, product)
+            price_without_tax = self._get_base_price(store, product, batch_context)
             attr_el = etree.SubElement(attributes_el, "attribute")
             etree.SubElement(
                 attr_el, "name"
@@ -259,22 +431,35 @@ class CatalogController(http.Controller):
             for pricelist in store.additional_pricelist_ids:
                 if not pricelist.shoppingfeed_attribute_name:
                     continue
-                price = pricelist.currency_id.round(
-                    pricelist._get_product_price(product, 1.0, None)
-                )
+                if batch_context:
+                    price = batch_context["additional_prices"][pricelist.id].get(
+                        product.id, 0.0
+                    )
+                else:
+                    price = pricelist.currency_id.round(
+                        pricelist._get_product_price(product, 1.0, None)
+                    )
                 attr_el = etree.SubElement(attributes_el, "attribute")
                 etree.SubElement(
                     attr_el, "name"
                 ).text = pricelist.shoppingfeed_attribute_name
                 etree.SubElement(attr_el, "value").text = str(price)
 
+    def _build_metadata_xml(self, total_products, started_at):
+        metadata_el = etree.Element("metadata")
+        self._add_metadata_values(metadata_el, total_products, started_at)
+        return metadata_el
+
     def _add_metadata(self, catalog_el, total_products):
         metadata_el = etree.SubElement(catalog_el, "metadata")
+        self._add_metadata_values(metadata_el, total_products, datetime.now())
+
+    def _add_metadata_values(self, metadata_el, total_products, started_at):
         etree.SubElement(metadata_el, "platform").text = f"Odoo:{release.version}"
         etree.SubElement(
             metadata_el, "agent"
         ).text = f"shoppingfeed_integration:{release.version}"
-        etree.SubElement(metadata_el, "startedAt").text = datetime.now().isoformat()
+        etree.SubElement(metadata_el, "startedAt").text = started_at.isoformat()
         etree.SubElement(metadata_el, "finishedAt").text = datetime.now().isoformat()
         etree.SubElement(metadata_el, "invalid").text = "0"
         etree.SubElement(metadata_el, "ignored").text = "0"
